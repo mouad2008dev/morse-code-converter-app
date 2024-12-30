e58605475e52a9292111566e53953ce257528ccd
mouad <152200815+mouad2008dev@users.noreply.github.com>
2024-12-30 19:20:14 +0100

    Update MainActivity.kt
    using / instead of space between codes

--- toMorseCode Project/app/src/main/java/com/example/tomosecode/MainActivity.kt
+++ toMorseCode Project/app/src/main/java/com/example/tomosecode/MainActivity.kt
@@ -74,7 +74,7 @@ fun UIDesign() {
 
         '0' to "-----", '1' to ".----", '2' to "..---", '3' to "...--",
         '4' to "....-", '5' to ".....", '6' to "-....", '7' to "--...",
-        '8' to "---..", '9' to "----.", ' ' to " "
+        '8' to "---..", '9' to "----.", ' ' to "/"
     )
 
     val clipboard : ClipboardManager = LocalClipboardManager.current
